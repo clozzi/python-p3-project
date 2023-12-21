343ac0bf629b5cfba053351fc6680cc42ccae713
Chris Lozzi <clozzi12@gmail.com>
2023-12-21 16:12:55 -0500

    first cli submenu

--- lib/cli.py
+++ lib/cli.py
@@ -6,36 +6,46 @@ from helpers import (
     list_robots
 )
 
-def e_exit():
-    exit_program()
-
 def planet_menu():
-    list_planets()
+    while True:
+        list_planets()
 
-    print("Type planet # to see details")
-    print("Type A to add a planet")
-    print("Type B to go back to the previous menu")
-    print("Type E to exit")
+        print("Type planet # to see details")
+        print("Type A to add a planet")
+        print("Type B to go back to the previous menu")
+        print("Type E to exit")
+
+        planet_choice = input("> ")
+
+        if planet_choice == "A":
+            print("Add planet function")
+        elif planet_choice == "B":
+            main()
+        elif planet_choice == "E":
+            exit_program()
+        else:
+            print("Invalid choice.")
 
 def robot_menu():
     list_robots()
 
 def main():
-    print("Please select an option:")
-    print("E to Exit the program")
-    print("1. Explore all Planets")
-    print("2. Explore all Robots")
+    while True:
+        print("Please select an option:")
+        print("E to Exit the program")
+        print("1. Explore all Planets")
+        print("2. Explore all Robots")
 
-    choice = input("> ")
+        choice = input("> ")
 
-    if choice == "E":
-        e_exit()
-    elif choice == "1":
-        planet_menu()
-    elif choice == "2":
-        robot_menu()
-    else:
-        print("Invalid choice")
+        if choice == "E":
+            exit_program()
+        elif choice == "1":
+            planet_menu()
+        elif choice == "2":
+            robot_menu()
+        else:
+            print("Invalid choice")
 
 
 if __name__ == "__main__":
