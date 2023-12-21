585c402b22ea4834c065f5915ae15d22e1d8b731
Chris Lozzi <clozzi12@gmail.com>
2023-12-21 16:15:32 -0500

    added mars robot

--- lib/seed.py
+++ lib/seed.py
@@ -9,12 +9,13 @@ def seed_db():
     Planet.create_table()
     Robot.create_table()
 
-    earth = Planet.create("Earth", "solar")
-    mars = Planet.create("Mars", "solar")
+    earth = Planet.create("Earth", "Sol")
+    mars = Planet.create("Mars", "Sol")
     Robot.create("Walle", "terrestrial", earth.id)
     Robot.create("Johnny", "terrestrial", earth.id)
     Robot.create("Skynet", "aerial", earth.id)
     Robot.create("Ingenuity", "aerial", mars.id)
+    Robot.create("Sojourner", "terrestrial", mars.id)
 
 seed_db()
 print("DB seeded")
